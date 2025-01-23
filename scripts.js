8a5f6fa52a359093d2dd082bd0d7abbda958b3c3
Nikhil Ignatius <89331788+NikhilIgnattius@users.noreply.github.com>
2025-01-23 12:37:57 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,15 @@
+import contrastChanger from "./contrastChanger.js";
+
+function change(){
+
+  contrastChanger()
+}
+function highlightLinks() {
+  console.log("Links highlighted.");
+}
+
+function print(message) {
+  console.log(message);
+}
+
+document.getElementById("contrastChange").addEventListener("click",contrastChanger);
